4a4815f07244a73523554428c9fd17e2d4ef6ff7
shivamkathait19 <kathaitshivam151@gmail.com>
2026-03-19 22:09:24 +0530

    Update Mainpages.dart

---
+++ lib/Mainpages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class mainpage extends StatelessWidget {
+  const mainpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ();
+  }
+}
