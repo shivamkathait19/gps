efc5289972712dc05f4c32c46d0627008b2f53cb
shivamkathait19 <kathaitshivam151@gmail.com>
2026-03-25 22:24:27 +0530

    Add

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -7,13 +7,16 @@
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:geolocator_android/geolocator_android.dart' as geolocator_android;
-import 'package:google_maps_flutter_android/google_maps_flutter_android.dart' as google_maps_flutter_android;
+import 'package:path_provider_android/path_provider_android.dart' as path_provider_android;
 import 'package:geolocator_apple/geolocator_apple.dart' as geolocator_apple;
-import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart' as google_maps_flutter_ios;
+import 'package:path_provider_foundation/path_provider_foundation.dart' as path_provider_foundation;
 import 'package:geolocator_linux/geolocator_linux.dart' as geolocator_linux;
 import 'package:package_info_plus/package_info_plus.dart' as package_info_plus;
+import 'package:path_provider_linux/path_provider_linux.dart' as path_provider_linux;
 import 'package:geolocator_apple/geolocator_apple.dart' as geolocator_apple;
+import 'package:path_provider_foundation/path_provider_foundation.dart' as path_provider_foundation;
 import 'package:package_info_plus/package_info_plus.dart' as package_info_plus;
+import 'package:path_provider_windows/path_provider_windows.dart' as path_provider_windows;
 
 @pragma('vm:entry-point')
 class _PluginRegistrant {
@@ -31,10 +34,10 @@ class _PluginRegistrant {
       }
 
       try {
-        google_maps_flutter_android.GoogleMapsFlutterAndroid.registerWith();
+        path_provider_android.PathProviderAndroid.registerWith();
       } catch (err) {
         print(
-          '`google_maps_flutter_android` threw an error: $err. '
+          '`path_provider_android` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -50,10 +53,10 @@ class _PluginRegistrant {
       }
 
       try {
-        google_maps_flutter_ios.GoogleMapsFlutterIOS.registerWith();
+        path_provider_foundation.PathProviderFoundation.registerWith();
       } catch (err) {
         print(
-          '`google_maps_flutter_ios` threw an error: $err. '
+          '`path_provider_foundation` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -77,6 +80,15 @@ class _PluginRegistrant {
         );
       }
 
+      try {
+        path_provider_linux.PathProviderLinux.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isMacOS) {
       try {
         geolocator_apple.GeolocatorApple.registerWith();
@@ -87,12 +99,30 @@ class _PluginRegistrant {
         );
       }
 
+      try {
+        path_provider_foundation.PathProviderFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isWindows) {
       try {
         package_info_plus.PackageInfoPlusWindowsPlugin.registerWith();
       } catch (err) {
         print(
           '`package_info_plus` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        path_provider_windows.PathProviderWindows.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_windows` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
