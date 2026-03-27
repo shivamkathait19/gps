30c7159947898a466888c61b4d3a21971fd4d8b0
shivamkathait19 <kathaitshivam151@gmail.com>
2026-03-27 22:27:47 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.gps"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "25.2.9519653"
+    //ndkVersion = "25.2.9519653"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
