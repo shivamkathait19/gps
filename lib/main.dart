351afc3ba77560b9d1aa57770c543b42929a6db8
shivamkathait19 <kathaitshivam151@gmail.com>
2026-03-30 22:07:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:mainPages());
+      home:loginScreen()));
   }
 }
 
