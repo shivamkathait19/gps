b3d72e4b8f4e1f2fc8c33731ad25f3b53747a304
shivamkathait19 <kathaitshivam151@gmail.com>
2026-04-01 23:40:52 +0530

    error!

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+/*plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
@@ -7,10 +7,10 @@ plugins {
 
 android {
     namespace = "com.example.gps"
-    compileSdk = 36
+    compileSdk = 35
     // compileSdk = flutter.compileSdkVersion
    //ndkVersion = "27.0.12077973"
-    ndkVersion = "26.3.11579264"
+    ndkVersion = '29.0.13599879'
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -18,7 +18,8 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
+    //jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -27,7 +28,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 36 /*flutter.targetSdkVersion*/
+        targetSdk = 34 /*flutter.targetSdkVersion*/
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -37,6 +38,92 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace "com.example.gps"
+    compileSdk 35
+    ndkVersion = '29.0.13599879'
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_17
+                targetCompatibility JavaVersion.VERSION_17
+
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId "com.example.card_x"
+        minSdkVersion flutter.minSdkVersion
+                targetSdk 34
+        versionCode flutter.versionCode
+                versionName flutter.versionName
+                multiDexEnabled true
+    }
+
+    buildTypes {
+        release {
+            signingConfig signingConfigs.debug
+                    proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}*/
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.gps"
+    compileSdk = 35
+    ndkVersion = "26.3.11579264"   // ⚠️ stable version use karo
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.example.card_x"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
