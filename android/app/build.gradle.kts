7513e18b723fabdb83de2de3261efe3ff29a94d8
shivamkathait19 <kathaitshivam151@gmail.com>
2026-04-04 23:50:21 +0530

    still error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 36
    // compileSdk = flutter.compileSdkVersion
    ndkVersion = "27.0.12077973"
-   // ndkVersion = "29.0.13599879"
+   
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
