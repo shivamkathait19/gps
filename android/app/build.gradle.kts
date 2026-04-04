62b57873fbb016c65e811e277a4ce6809ebb4e9b
shivamkathait19 <kathaitshivam151@gmail.com>
2026-04-05 00:09:42 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
 
 android {
     namespace = "com.example.gps"
-    compileSdk = 36
-   // compileSdk = flutter.compileSdkVersion
-   ndkVersion = "27.0.12077973"
+    //compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
    
 
     compileOptions {
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 34 /*flutter.targetSdkVersion*/
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
