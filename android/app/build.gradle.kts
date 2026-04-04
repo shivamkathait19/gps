d3f17d6c19745cd908ef61967cc91d1cb8f8e5ca
shivamkathait19 <kathaitshivam151@gmail.com>
2026-04-04 23:36:06 +0530

    errro! return

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,9 +8,9 @@ plugins {
 android {
     namespace = "com.example.gps"
     compileSdk = 36
-   //  compileSdk = flutter.compileSdkVersion
-   //ndkVersion = "27.0.12077973"
-    ndkVersion = "29.0.13599879"
+   // compileSdk = flutter.compileSdkVersion
+   ndkVersion = "27.0.12077973"
+   // ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
