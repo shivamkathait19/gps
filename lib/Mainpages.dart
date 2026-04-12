f956921033fe70b1d3bbd6eff6b8e3fda067c141
shivamkathait19 <kathaitshivam151@gmail.com>
2026-04-12 16:41:16 +0530

    Add

--- lib/Mainpages.dart
+++ lib/Mainpages.dart
@@ -21,7 +21,7 @@ class Mainpages extends StatelessWidget {
             )
             ),
            Positioned(
-             top: 15,
+             top: 50,
                left: 15,
                right: 15,
                child: Container(
@@ -37,142 +37,79 @@ class Mainpages extends StatelessWidget {
              ),
                  child: Row(
                    children: [
-                    Builder(builder: (context)=> IconButton(onPressed: (){}, icon:Icon(Icons.menu))),
-                     TextFormField()
-                     ]
+                     Builder(builder: (context)=> IconButton(onPressed: (){
+                       Scaffold.of(context).openDrawer();
+                     },
+                         icon: Icon(Icons.menu,color: Colors.black,))),
+                    SizedBox(width: 100,),
+                    Expanded(
+                        child: Center(
+                          child: TextField(
+                            decoration: InputDecoration(
+                           labelText: "Search here  ",labelStyle: TextStyle(
+                              color: Colors.black
+                            ),
+                           border: InputBorder.none,),),)),
+                      Icon(Icons.mic,color: Colors.black,),
+
+                   ],
+
                  ),
            ),
 
-           )
-
-          ],
-      ),
-    );
-  }
-}
-/*import 'package:flutter/material.dart';
-
-class Mainpages extends StatelessWidget {
-  const Mainpages({super.keyDra});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        backgroundColor: Colors.black,
-      ),
-
-      body: Stack(
-        children: [
- // 🗺️ Fake Map Background
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  "https://www.mysanantonio.com/business/article/Internet-Makes-Fake-Google-Maps-Location-for-16555487.php", // map style image
-                ),
-                fit: BoxFit.cover,
+           ),
+              Center(
+                child: Icon(Icons.location_on,color: Colors.red,size: 50,),
               ),
-            ),
-          ),
-
-          // 🔍 Search Bar (Top)
-          Positioned(
-            top: 50,
-            left: 15,
-            right: 15,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(color: Colors.black26, blurRadius: 5)
-                ],
-              ),
-              child: Row(
-                children: [
-                  Icon(Icons.menu),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: Text(
-                      "Search here...",
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                  Icon(Icons.mic),
-                ],
-              ),
-            ),
-          ),
-
-          // 📍 Center Marker
-          Center(
-            child: Icon(
-              Icons.location_on,
-              color: Colors.red,
-              size: 50,
-            ),
-          ),
-
-          // 🎯 Right Side Buttons
-          Positioned(
-            right: 15,
-            bottom: 120,
-            child: Column(
+            Positioned(
+              right: 20,
+              bottom: 90,
+              child: Column(
               children: [
                 FloatingActionButton(
                   mini: true,
                   onPressed: () {},
-                  child: Icon(Icons.my_location),
+                  child: Icon(Icons.my_location,size: 40,),
                 ),
-                SizedBox(height: 10),
+                 FloatingActionButton(
+                   mini: true,
+                   onPressed: (){},
+                 child: Icon(Icons.add,size: 40,),
+                 ),
                 FloatingActionButton(
-                  mini: true,
-                  onPressed: () {},
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(height: 10),
-                FloatingActionButton(
-                  mini: true,
-                  onPressed: () {},
-                  child: Icon(Icons.remove),
-                ),
+                  mini:  true,
+                  onPressed: (){},
+                child: Icon(Icons.remove,blendMode: BlendMode.modulate
+                  ,),
+                )
               ],
             ),
-          ),
-
-          // 📦 Bottom Card
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 100,
-              padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(color: Colors.black26, blurRadius: 5)
-                ],
-              ),
-              child: Row(
-                children: [
-                  Icon(Icons.location_on, color: Colors.red),
-                  SizedBox(width: 10),
-                  Text("Your Location"),
-                ],
-              ),
             ),
-          ),
-        ],
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+               height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical( top: Radius.circular(20)
+                    ),
+                    //backgroundBlendMode: BlendMode.colorDodge
+                  ),
+                      child: TextButton.icon(onPressed: (){},
+                          icon: Icon(Icons.location_on,color: Colors.red,),
+                          label: Text("your location ",style: TextStyle(
+                            color: Colors.red.shade800
+                          ),),),
+
+            ))
+          ],
       ),
+
+
     );
   }
 }
 
- */
+
