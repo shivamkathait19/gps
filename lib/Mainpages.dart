f041cbb1e9bcb2da1156f88542fc10346539a07b
shivamkathait19 <kathaitshivam151@gmail.com>
2026-04-15 21:30:12 +0530

    Add

--- lib/Mainpages.dart
+++ lib/Mainpages.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mainpages extends StatefulWidget {
   const Mainpages({super.key});
@@ -9,122 +9,147 @@ class Mainpages extends StatefulWidget {
 }
 
 class _MainpagesState extends State<Mainpages> {
+
+  late GoogleMapController mapController;
+
+  final LatLng _initialPosition = const LatLng(28.6139, 77.2090); // Delhi
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     drawer: Drawer(
-       backgroundColor: Colors.black,
-     ),
-      body: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShFjcpjTmNukIRjEMU1ltR-NAHXvqTXgTlrA&s"),
-             fit: BoxFit.cover
-              ),
-            )
-            ),
-           Positioned(
-             top: 50,
-               left: 15,
-               right: 15,
-               child: Container(
-                 margin: EdgeInsets.all(10),
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            decoration: BoxDecoration(
-             color: Colors.black.withOpacity(0.5),
-              borderRadius: BorderRadius.circular(30),),
-                 height: 50,
-                 child: Row(
-                   children: [
-                     Builder(builder: (context)=> IconButton(onPressed: (){
-                       Scaffold.of(context).openDrawer();
-                     },
-                         icon: Icon(Icons.menu,color: Colors.black,))),
-                    SizedBox(width: 100,),
-                    Expanded(
-                        child: Center(
-                          child: TextField(
-                            decoration: InputDecoration(
-                           labelText: "Search here  ",labelStyle: TextStyle(
-                              color: Colors.white
-                            ),
-                           border: InputBorder.none, /*enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white)
-                            )*/ ),),)),
-                      Icon(Icons.mic,color: Colors.black,),
-
-                   ],
-
-                 ),
-           ),
-
-           ),
-              Center(
-                child: Icon(Icons.location_on,color: Colors.red,size: 40,),
-              ),
-            Positioned(
-              right: 20,
-              bottom: 90,
-              child: Column(
-              children: [
-                FloatingActionButton(
-                  mini: true,
-                  onPressed: () {},
-                  child: Icon(Icons.my_location,size: 40,),
-                ),
-                 FloatingActionButton(
-                   mini: true,
-                   onPressed: (){},
-                 child: Icon(Icons.add,size: 40,),
-                 ),
-                FloatingActionButton(
-                  mini:  true,
-                  onPressed: (){},
-                child: Icon(Icons.remove,blendMode: BlendMode.modulate
-                  ,),
-                ),
-                FloatingActionButton(onPressed: (){},
-                  mini: true,
-                  child: Icon(Icons.photo,color: Colors.black,size: 20,),)
-              ],
-            ),
-            ),
-            Positioned(
-                bottom: 0,
-                right: 0,
-                left: 0,
-                child: Column(
-              children: [
-
-              ],
-            )),
-            Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-               height: 80,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.vertical( top: Radius.circular(20)
-                    ),
-                    backgroundBlendMode: BlendMode.colorDodge
-                  ),
-                      child: TextButton.icon(onPressed: (){},
-                          icon: Icon(Icons.location_on,color: Colors.red,),
-                          label: Text("your location ",style: TextStyle(
-                            color: Colors.red.shade800
-                          ),),),
-
-            ))
-          ],
+      drawer: Drawer(
+        backgroundColor: Colors.black,
       ),
+      body: Stack(
+        children: [
 
+          /// 🔥 GOOGLE MAP
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: _initialPosition,
+              zoom: 14,
+            ),
+            onMapCreated: (controller) {
+              mapController = controller;
+            },
+            myLocationEnabled: true,
+            myLocationButtonEnabled: false,
+            zoomControlsEnabled: false,
+          ),
 
+          /// 🔍 SEARCH BAR
+          Positioned(
+            top: 50,
+            left: 15,
+            right: 15,
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              height: 50,
+              child: Row(
+                children: [
+                  Builder(
+                    builder: (context) => IconButton(
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                      icon: const Icon(Icons.menu, color: Colors.white),
+                    ),
+                  ),
+
+                  const SizedBox(width: 10),
+
+                  const Expanded(
+                    child: TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: "Search here...",
+                        hintStyle: TextStyle(color: Colors.white70),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+
+                  const Icon(Icons.mic, color: Colors.white),
+                ],
+              ),
+            ),
+          ),
+
+          /// 📍 CENTER PIN
+          const Center(
+            child: Icon(Icons.location_on, color: Colors.red, size: 40),
+          ),
+
+          /// 🎯 FLOATING BUTTONS
+          Positioned(
+            right: 20,
+            bottom: 120,
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  mini: true,
+                  onPressed: () {
+                    mapController.animateCamera(
+                      CameraUpdate.newLatLng(_initialPosition),
+                    );
+                  },
+                  child: const Icon(Icons.my_location),
+                ),
+                const SizedBox(height: 10),
+
+                FloatingActionButton(
+                  mini: true,
+                  onPressed: () {
+                    mapController.animateCamera(
+                      CameraUpdate.zoomIn(),
+                    );
+                  },
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(height: 10),
+
+                FloatingActionButton(
+                  mini: true,
+                  onPressed: () {
+                    mapController.animateCamera(
+                      CameraUpdate.zoomOut(),
+                    );
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ),
+
+          /// 📍 BOTTOM LOCATION BAR
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 80,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.location_on, color: Colors.red),
+                label: Text(
+                  "Your Location",
+                  style: TextStyle(color: Colors.red.shade800),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
