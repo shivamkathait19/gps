a6a32d2ab88679b1386f895f1f5e5eae66e7813e
shivamkathait19 <kathaitshivam151@gmail.com>
2026-04-19 23:45:10 +0530

    Add

---
+++ lib/splash Screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+ class splasgScreen extends StatelessWidget {
+   const splasgScreen({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title: Text("Gogo Gps "),
+       ),
+     );
+   }
+ }
+ 
