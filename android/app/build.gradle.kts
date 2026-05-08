8891994059440f42554d382a54d57723d81595ad
shivamkathait19 <kathaitshivam151@gmail.com>
2026-05-08 23:03:38 +0530

    Add

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,10 +2,10 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-  //  id("com.google.gms.google-services") version "4.4.4" apply false
+   id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.gms.google-services")
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    //id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 
@@ -57,4 +57,4 @@ flutter {
     source = "../.."
 }
 
-
+apply plugin: 'com.google.gms.google-services'
