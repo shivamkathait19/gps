02442336d657620d013a1701fdfda555f543710d
shivamkathait19 <kathaitshivam151@gmail.com>
2026-06-25 21:24:41 +0530

    Add

--- lib/files/thired.dart
+++ lib/files/thired.dart
@@ -1,24 +1,77 @@
 import 'package:flutter/material.dart';
 
-class Thired extends StatelessWidget {
+class Thired extends StatefulWidget {
   const Thired({super.key});
+
+  @override
+  State<Thired> createState() => _ThiredState();
+}
+
+class _ThiredState extends State<Thired> {
+  bool cemeraAccess = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SafeArea(child: Column(
-        children: [
+      body:SafeArea(child: Padding(
+        padding:  EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+             SizedBox(height: 19,),
+            Text("We need some access!" ,textAlign: TextAlign.center,style: TextStyle(
+              color: Colors.amber, fontSize: 28,
+            ),),
+            Text("You need to grant access to the device camera."
+                " Photo Library and Location to take photos ore record video", textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18,color: Colors.black, fontWeight: FontWeight.w400 ),
+            ),
+             Container(
+               decoration: BoxDecoration(
+                 color: Colors.white,
+                 borderRadius: BorderRadius.circular(16),
+                 border: Border.all(
+                   color: Colors.grey.shade100
 
-          Text("We need some access!" ,textAlign: TextAlign.center,style: TextStyle(
-            color: Colors.amber, fontSize: 32,
-          ),),
-          Text("You need to grant access to the device camera."
-              " Photo Library and Location to take photos ore record video", textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 18,color: Colors.black, fontWeight: FontWeight.bold ),
-          ),
+                 ),
+               ),
+               child: Row(
+                 children: [
+                   CircleAvatar(
+                     radius: 28,
+                     backgroundColor: Colors.amber,
+                     child: Icon(Icons.camera_alt,
+                       color: Colors.orange,size: 32,),
+                   ),
+                   Expanded(child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: [
+                       Text("Cemera Access",style: TextStyle(
+                         fontSize: 22,
+                         fontWeight: FontWeight.bold,
 
-          
-        ],
+                       ),),
+                       Text("App needs access to your cemera for capture photo & videos",style: TextStyle(
+                         color: Colors.grey.shade600,
+                         fontSize: 15,
+                       ),
+                       ),
+                       Switch(value: cemeraAccess,
+                         activeColor: Colors.amber,
+                         onChanged: (value){
+                         setState((){
+                           cemeraAccess = value;
+                         });
+                         },
+                       )
+                     ],
+                   )),
+
+                 ],
+               ),
+             )
+
+          ],
+        ),
       )),
     );
   }
