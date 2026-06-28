538d334bd153788c2ace8d2b727e1bd1856c20fa
shivamkathait19 <kathaitshivam151@gmail.com>
2026-06-28 21:41:19 +0530

    Update splash Screen.dart

--- lib/files/splash Screen.dart
+++ lib/files/splash Screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:gps/files/second.dart';
  class splasgScreen extends StatefulWidget {
@@ -16,7 +18,19 @@ class _splasgScreenState extends State<splasgScreen>
     // TODO: implement initState
     super.initState();
             _controller = AnimationController(vsync: this, duration: Duration(seconds:1))..repeat(reverse: true);
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const second(),
+        ),
+      );
+    });
+      }
   }
+
+
 
    @override
    Widget build(BuildContext context) {
