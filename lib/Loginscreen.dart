36934a4c5e7819ca6cb2b4007bc53da390c84a1f
shivamkathait19 <kathaitshivam151@gmail.com>
2026-07-24 23:06:55 +0530

    Update Loginscreen.dart

--- lib/Loginscreen.dart
+++ lib/Loginscreen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:gps/Mainpages.dart';
+import 'package:gps/files/Mainpages.dart';
 import 'package:gps/mainform.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -411,8 +412,7 @@ class _LoginScreenState extends State<LoginScreen>with SingleTickerProviderState
                             });
 
                             Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (_) => Mainpage()),
+                              context, MaterialPageRoute(builder: (_) => Mainpage()),
                             );
                           },
                           child: Text("Login"),
